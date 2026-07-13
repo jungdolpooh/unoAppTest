f6b4a0ab673106be6b7c5e39f6076cbbca7fc0e4
jwlee <jwlee@smartmaker.com>
2026-07-13 13:57:13 +0900

    Fix diagonal gradient on WASM color picker sliders

--- service-worker.js
+++ service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('958d12d8-a504-478c-9265-6fd8596e123b').then(async function (cache) {
+            caches.open('97cd7ca7-bc74-438e-875a-b54f16a7d136').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '958d12d8-a504-478c-9265-6fd8596e123b';
+                        return cacheName !== '97cd7ca7-bc74-438e-875a-b54f16a7d136';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
