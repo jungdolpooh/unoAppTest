d79c478bb8d6d148256b678a8eb9ed9b01e1b626
jwlee <jwlee@smartmaker.com>
2026-07-14 14:45:10 +0900

    Update deployment

--- service-worker.js
+++ service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('568c2a3a-8d9c-4d87-b6f4-19b5f5d05cbb').then(async function (cache) {
+            caches.open('f94cfb0c-1047-4e10-8de6-69a688c6ab85').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '568c2a3a-8d9c-4d87-b6f4-19b5f5d05cbb';
+                        return cacheName !== 'f94cfb0c-1047-4e10-8de6-69a688c6ab85';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
